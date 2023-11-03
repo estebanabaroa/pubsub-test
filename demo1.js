ca78c1224670dc30afd064c1b0eea21c6b0172e6
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-11-03 13:52:45 +0000

    add more logs

--- demo1.js
+++ demo1.js
@@ -1,12 +1,31 @@
 import { createLibp2p } from 'libp2p'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { tcp } from '@libp2p/tcp'
-import { webSockets } from '@libp2p/websockets'
 import { mplex } from '@libp2p/mplex'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { noise } from '@chainsafe/libp2p-noise'
 import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
 import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
+
+const logEvents = (nodeName, node) => {
+    const events = [
+        'connection:close',
+        'connection:open',
+        'connection:prune',
+        'peer:connect',
+        'peer:disconnect',
+        'peer:discovery',
+        'peer:identify',
+        'peer:update',
+        'self:peer:update',
+        'start',
+        'stop',
+        'transport:close',
+        'transport:listening',
+    ]
+    const logEvent = (event) => console.log(nodeName, event.type, event.detail)
+    events.forEach(event => node.addEventListener(event, logEvent))
+}
 
 const createNode1 = async () => {
     const node = await createLibp2p({
@@ -24,9 +43,11 @@ const createNode1 = async () => {
             })
         }
     })
+    logEvents('node1', node)
     return node
 }
 const node1 = await createNode1()
+console.log(node1.getMultiaddrs())
 
 const createNode2 = async () => {
     const node = await createLibp2p({
@@ -44,6 +65,7 @@ const createNode2 = async () => {
             })
         }
     })
+    logEvents('node2', node)
     return node
 }
 const node2 = await createNode2()
@@ -51,7 +73,7 @@ const node2 = await createNode2()
 // log addresses
 console.log(node1.getMultiaddrs(), node2.getMultiaddrs())
 
-const topic = 'news'
+const topic = 'demo'
 
 // sub
 node1.services.pubsub.addEventListener('message', (evt) => {
@@ -65,12 +87,12 @@ await node2.services.pubsub.subscribe(topic)
 
 // pub
 setInterval(() => {
-  node2.services.pubsub.publish(topic, uint8ArrayFromString('Bird bird bird, bird is the word!')).catch(err => {
+  node2.services.pubsub.publish(topic, uint8ArrayFromString('demo message from node 2')).catch(err => {
     console.error(err)
   })
 }, 1000)
 setInterval(() => {
-  node1.services.pubsub.publish(topic, uint8ArrayFromString('Bear bear bear, bear is the word!')).catch(err => {
+  node1.services.pubsub.publish(topic, uint8ArrayFromString('demo message from node 1')).catch(err => {
     console.error(err)
   })
 }, 2000)
