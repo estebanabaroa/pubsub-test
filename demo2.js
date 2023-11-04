66944f9445e79f9a8997092b949e8d773a5dd1a5
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-11-04 15:50:24 +0000

    update

--- demo2.js
+++ demo2.js
@@ -1,4 +1,3 @@
-// import util from 'util'
 import { createLibp2p } from 'libp2p'
 import { peerIdFromString } from '@libp2p/peer-id'
 import { multiaddr } from '@multiformats/multiaddr'
@@ -10,9 +9,33 @@ import { noise } from '@chainsafe/libp2p-noise'
 import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
 import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
 
-// util.inspect.defaultOptions.depth = null
-
-;(async () => {
+const log = (...args) => {
+    console.log(...args)
+    try {
+        const p = document.createElement('p')
+        let textContent = ''
+        for (const [i, arg] of args.entries()) {
+            if (textContent !== '') {
+                textContent += ' '
+            }
+            if (typeof arg === 'object') {
+                textContent += JSON.stringify(arg, null, 2)
+                if (args.length - 1 !== i) {
+                    textContent += '\n'
+                }
+            }
+            else {
+                textContent += arg
+            }
+        }
+        p.textContent = textContent
+        p.style.whiteSpace = 'pre'
+        document.body.appendChild(p)
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
 
 const logEvents = (nodeName, node) => {
     const events = [
@@ -30,14 +53,25 @@ const logEvents = (nodeName, node) => {
         'transport:close',
         'transport:listening',
     ]
-    const logEvent = (event) => console.log(nodeName, event.type, event.detail)
+    const logEvent = (event) => log(nodeName, event.type, event.detail)
     events.forEach(event => node.addEventListener(event, logEvent))
 }
 
+let peerId, peerMultiAddress
+try {
+    // example /ip4/0.0.0.0/udp/36338/quic-v1/webtransport/certhash/uEiAKYNN-nS53kXTQTzM0__ksuMI9mEczZA9YbNyyth-NLw/certhash/uEiD9Ja-UEHkuLjtaVKX7D1wZVri6GKkrmxFOue8-9YMAzg/p2p/Qmbi4zsa9H8WDZk8akwSiBqEe8U1Gedh8Mh4R4aCfR3Twx
+    const split = location.search.replace(/^\?/, '').split('/p2p/')
+    peerId = split[1]
+    peerMultiAddress = split[0]
+}
+catch (e) {}
+
+;(async () => {
+
 // node1 is created in go because listening on webTransport is not implemented in libp2p js
 const node1 = {
-    peerId: peerIdFromString('12D3KooWMbbPVGsZYh3ChQjue712NHHGNybRRXwnuSpezYjGbCDS'),
-    getMultiaddrs: () => [multiaddr('/ip4/0.0.0.0/udp/9999/quic-v1/webtransport/certhash/uEiBHtBgYlLSKhAzfyP8WQs9SbCVLWmYx3YZ_TeAuOB2Tyg/certhash/uEiC1wrlGK_ZlqKv1Xh2FTmrjQCAU5SKvoKfzTabitWYSPQ')]
+    peerId: peerIdFromString(peerId),
+    getMultiaddrs: () => [multiaddr(peerMultiAddress)]
 }
 
 const createNode2 = async () => {
@@ -61,13 +95,13 @@ const createNode2 = async () => {
 const node2 = await createNode2()
 
 // log addresses
-console.log(node1.getMultiaddrs(), node2.getMultiaddrs())
+log('node1', node1.getMultiaddrs(), 'node2', node2.getMultiaddrs())
 
 const topic = 'demo'
 
 // sub
 node2.services.pubsub.addEventListener('message', (evt) => {
-  console.log(`node2 received: ${uint8ArrayToString(evt.detail.data)} on topic ${evt.detail.topic}`)
+    log(`node2 received: ${uint8ArrayToString(evt.detail.data)} on topic ${evt.detail.topic}`)
 })
 await node2.services.pubsub.subscribe(topic)
 
