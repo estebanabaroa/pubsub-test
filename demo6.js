4594fe0227a5083b9c5e73934b71a916f2d847df
Esteban Abaroa <estebanabaroa@protonmail.com>
2024-02-23 22:12:49 +0000

    1

--- demo6.js
+++ demo6.js
@@ -15,7 +15,7 @@ import { webRTCDirect } from '@libp2p/webrtc'
 import { webSockets } from '@libp2p/websockets'
 // import { circuitRelayTransport } from '@libp2p/circuit-relay-v2'
 
-document.title = 'v6'
+document.title = 'v7'
 
 const log = (...args) => {
     console.log(...args)
